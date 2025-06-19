bc49fbad6f495762fc11033b2e745c24f4ec2b02
Alejandro Lopez Correa <allopezcorrea@gmail.com>
2025-06-19 07:20:23 -0500

    Init commit, g authentication, fragment caching and shared partial caching

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,56 @@
+class ProductsController < ApplicationController
+  before_action :product, only: %i[show edit update destroy]
+
+  def index
+    # low level caching
+
+    Rails.cache.fetch("products", expires_in: 12.hours) do
+      @pagy, @products = pagy(Product.all)
+    end
+  end
+
+  def show
+    product
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if product.update(product_params)
+      redirect_to product
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    product.destroy
+
+    redirect_to products_path
+  end
+
+  private
+
+  def product
+    @product ||= Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name)
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
+require "faker"
+
+User.destroy_all
+Product.destroy_all
+
+User.create!(
+  email_address: "admin@example.com",
+  password: "password"
+)
+
+100.times do
+  Product.create!(
+    name: Faker::Commerce.product_name
+  )
+end
